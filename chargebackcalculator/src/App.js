6494a04e3537ebfe4f4e58c477f9300764be2ca1
Coding Tricks <codingtricks@gmail.com>
2022-02-01 01:07:42 +0530

    first commit

---
+++ chargebackcalculator/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello, <code>Himanshu Gupta</code> and team.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/codingtrick/chargeback-calculator-frontend"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open GitHub Repo
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
