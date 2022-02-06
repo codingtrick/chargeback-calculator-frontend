39991bf74c4c269445d9be1dc4b65b9668772891
Coding Tricks <codingtricks@gmail.com>
2022-02-07 01:00:36 +0530

    testing gitlab

--- chargebackcalculator/src/components/login.components.js
+++ chargebackcalculator/src/components/login.components.js
@@ -15,24 +15,42 @@ const UserInput = (props) => {
   );
 };
 
-const LoginComponents = () => {
-  console.log("hello compoents");
+// const LoginComponents = () => {
+//   console.log("hello compoents");
+//   return (
+//     <div>
+//       <h1>Hello from Login Components</h1>
+//       <UserInput
+//         texttype={"text"}
+//         id={"username"}
+//         name={"username"}
+//         placeholder={"enter username..."}
+//       />
+
+//       <UserInput
+//         texttype={"password"}
+//         id={"password"}
+//         name={"password"}
+//         placeholder={"enter password..."}
+//       />
+//     </div>
+//   );
+// };
+const ExpINput = (props) => {
+  return (
+    <>
+      <input type={props.type} placeholder={props.placeholder} />
+    </>
+  );
+};
+
+const LoginComponents = (props) => {
   return (
     <div>
-      <h1>Hello from Login Components</h1>
-      <UserInput
-        texttype={"text"}
-        id={"username"}
-        name={"username"}
-        placeholder={"enter username..."}
-      />
-
-      <UserInput
-        texttype={"password"}
-        id={"password"}
-        name={"password"}
-        placeholder={"enter password..."}
-      />
+      <ExpINput type={"text"} placeholder={"hk"} />
+      <ExpINput type={"text"} placeholder={"hk"} />
+      <ExpINput type={"text"} placeholder={"hk"} />
+      {/* testing */}
     </div>
   );
 };
